f2b9d63cd0127d9d8d786791dda76993df48945e
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2016-10-11 14:27:51 +0200

    Embed Logger within DebugLogger

--- dlog.go
+++ dlog.go
@@ -1,10 +1,33 @@
 package dlog
 
 import (
+	"io"
 	"log"
 )
 
 var debug bool = false
+
+type DebugLogger struct {
+	*log.Logger
+	debugEnabled bool
+}
+
+func New(out io.Writer, prefix string, flag int, debugEnabled bool) *DebugLogger {
+	return &DebugLogger{log.New(out, prefix, flag), debugEnabled}
+}
+
+func (d *DebugLogger) Enable()  { d.debugEnabled = true }
+func (d *DebugLogger) Disable() { d.debugEnabled = false }
+
+func (d *DebugLogger) Debug(args ...interface{}) {
+	if d.debugEnabled {
+		d.Print("[DEBUG]", args)
+	}
+}
+
+func (d *DebugLogger) Enter(fn string) {
+	d.Debug("ENTER:", fn)
+}
 
 // TODO: Integrate debug control into the logger instead of having to have ugly if statements directly in the code
 // TODO: Extend the logger even further to also have ready generic functionality to log function ENTRY/EXIT
